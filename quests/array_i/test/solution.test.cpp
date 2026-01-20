41292cee121e6bbbd53810126ca0507856f3795a
Dean Wilson <mail@deanwilson.dev>
2026-01-20 16:12:06 +0800

    initial commit

---
+++ quests/array_i/test/solution.test.cpp
@@ -0,0 +1,8 @@
+#include "umbra/test_cpp.hpp"
+#include <umbra/test.h>
+
+DESCRIBE("Array I Tests", {
+  TEST("TEST CASE 1", [](void *user) { (void)user; 
+      ASSERT_TRUE(true)
+      });
+});
---
+++ runners/run_all_tests.cpp
@@ -0,0 +1,9 @@
+#include <umbra/test.h>
+
+int main() {
+  TestRunner runner;
+  test_runner_init(&runner);
+  TestRegistry *registry = test_default_registry();
+  TestRunSummary summary = test_runner_run_all(&runner, registry);
+  return (summary.failed == 0) ? 0 : 1;
+}
