0fccaae3362f63ad6174a7ea9c015887f2b5d18d
Dean Wilson <mail@deanwilson.dev>
2026-01-22 16:11:26 +0800

    testing framework implementation working

--- quests/array_i/src/solution.cpp
+++ quests/array_i/src/solution.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-  std::vector<int> getConcatenation(std::vector<int> &nums) {
+  static std::vector<int> getConcatenation(std::vector<int> &nums) {
     std::vector<int> ans = nums;
     for (int i = 0; i < nums.size(); i++) {
       ans.push_back(nums[i]);
--- quests/array_i/test/solution.test.cpp
+++ quests/array_i/test/solution.test.cpp
@@ -1,8 +1,26 @@
-#include "umbra/test_cpp.hpp"
-#include <umbra/test.h>
+#include "../src/solution.cpp"
+#include <umbra/test_cpp.hpp>
+#include <vector>
 
 DESCRIBE("Array I Tests", {
-  TEST("TEST CASE 1", [](void *user) { (void)user; 
-      ASSERT_TRUE(true)
-      });
+  TEST("TEST CASE 1", {
+    (void)user;
+    std::vector<int> nums = {1, 2, 1};
+    std::vector<int> result = Solution::getConcatenation(nums);
+    std::vector<int> expected = {1, 2, 1, 1, 2, 1};
+    // MAIN QUEST: Add Assertions for array comparisons
+
+    // ASSERT_EQUAL_INT(expected, result);
+    ASSERT_TRUE(false);
+  });
+
+  TEST("TEST CASE 2", {
+    (void)user;
+    std::vector<int> nums = {1, 3, 2, 1};
+    std::vector<int> result = Solution::getConcatenation(nums);
+    std::vector<int> expected = {1, 3, 2, 1, 1, 3, 2, 1};
+
+    // ASSERT_EQUAL_INT(expected, result);
+    ASSERT_TRUE(false);
+  });
 });
